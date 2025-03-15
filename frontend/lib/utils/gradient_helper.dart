d49e3147404b6fbfc082f7683dec3ce84105329e
Pray-yzzzz <jx222yz528@outlook.com>
2025-03-15 14:21:05 +0800

    背景似乎变得好看了一点

--- frontend/lib/utils/gradient_helper.dart
+++ frontend/lib/utils/gradient_helper.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-const Color primaryColor = Color.fromRGBO(255, 105, 120, 0.6);
-const Color primaryGradientColor = Color.fromRGBO(114, 194, 255, 0.6);
+const Color primaryColor = Color.fromRGBO(253, 92, 108, 0.6);
+const Color primaryGradientColor = Color.fromRGBO(133, 200, 251, 0.6);
 
 RadialGradient createPrimaryGradient() {
   return RadialGradient(
     center: Alignment(0.5, 0.5), // 静态的中心点
     radius: 1.2,
-    colors: const [primaryColor, primaryGradientColor],
-    stops: const [0.3, 1.0],
+    colors: const [
+      Color.fromARGB(136, 244, 128, 146),
+      Color.fromARGB(168, 102, 178, 241),
+    ],
+    stops: const [0.2, 1.0],
   );
 }
